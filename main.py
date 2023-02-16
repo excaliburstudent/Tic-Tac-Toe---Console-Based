88cc96af8e48a5ea68f8f25bc7cb8a82c85821b4
EssaStudent <123898297+excaliburstudent@users.noreply.github.com>
2023-02-16 12:46:08 +0000

    Add function to get the computer's choice

--- main.py
+++ main.py
@@ -7,6 +7,9 @@
 # Author: Robert Walsh
 # Date Created: January 29, 2023
 
+import random
+import time
+
 EMPTY = "empty"
 COMPUTER = "X"
 PLAYER = "O"
@@ -62,8 +65,22 @@ def get_player_choice(the_board):
         valid_choice = True
   return choice
 
+def get_computer_choice(the_board):
+  print("It's my turn.")
+  time.sleep(random.randint(25, 75)/100.0)
+  choice = random.randint(1, 9)
+  while the_board[choice - 1] != EMPTY:
+    time.sleep(random.randint(25, 50)/100.0)
+    choice = random.randint(1, 9)
+
+  return choice
+
+random.seed()
 whose_turn = greet_player()
 draw_board(board)
 
-player_choice = get_player_choice(board)
-print("You chose: {}".format(player_choice))
+#player_choice = get_player_choice(board)
+#print("You chose: {}".format(player_choice))
+
+computer_choice = get_computer_choice(board)
+print("I chose {}".format(computer_choice))
